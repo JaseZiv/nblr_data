0453a721bc7ed34ef0c11603516bc197f5c08cf3
Jason Zivkovic <jaseziv83@gmail.com>
2026-01-18 15:28:56 +1100

    another dirty pbp game

--- update/update-26/update_pbp.R
+++ update/update-26/update_pbp.R
@@ -7,7 +7,8 @@ current_season <- "2025-2026"
 bad_pbp <- c("b1b1def4-4bef-11f0-8dce-5f21da19a0fe", "b1c59937-4bef-11f0-9850-69185882ba54", "b20bc78d-4bef-11f0-9d84-e3ab37cba7df", 
              "b22c940a-4bef-11f0-b917-b31681696f40", "b204326e-4bef-11f0-a4c0-7d8c00260a69", "b2317162-4bef-11f0-b94a-2b5bbafc199b",
              "b21aeb47-4bef-11f0-be7c-3fd9cdc8f1b9", "b217a699-4bef-11f0-bc08-933162362355", "b225ec91-4bef-11f0-ab26-f7367f0af8fe",
-             "b245e256-4bef-11f0-a465-3bb81d630de9", "b25032fc-4bef-11f0-9f55-d922ae3738da", "b2521ec7-4bef-11f0-868d-35d49e4d3f0e")
+             "b245e256-4bef-11f0-a465-3bb81d630de9", "b25032fc-4bef-11f0-9f55-d922ae3738da", "b2521ec7-4bef-11f0-868d-35d49e4d3f0e",
+             "b24b6e4c-4bef-11f0-baa2-55e0c73fcc94")
 
 player_meta <- all_season |> 
   # head(1) |> 
